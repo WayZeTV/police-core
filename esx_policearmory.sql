fa7f6f283da8a0631be1f6a992f7a406ea96fffb
WayZe <wayzeprofessionnel@gmail.com>
2022-08-10 18:26:08 +0200

    Delete esx_policearmory.sql

--- esx_policearmory.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `user_policeArmory` (
-  `identifier` varchar(50) COLLATE utf8mb4_bin NOT NULL,
-  `weapons` longtext COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
